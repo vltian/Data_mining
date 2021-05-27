009d8191726874048eb7f20fdb1ee9c27f1c306d
vltian <vltian@mail.ru>
2021-05-28 00:55:23 +0300

    HW1

--- HW1_parse.py
+++ HW1_parse.py
@@ -1,58 +1,27 @@
-import json
-import time
-from pathlib import Path
 import requests
+import json
+from pathlib import Path
 
-class Parse:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
-    }
-    params = {
-    }
+def get_response(url):
+    while True:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return response
+        time.sleep(0.5)
 
-    def __init__(self, url, save_path: Path):
-        self.url = url
-        self.save_path = save_path
-
-    def _get_response(self, url):
-        while True:
-            response = requests.get(url, params=self.params, headers=self.headers)
-            if response.status_code == 200:
-                return response
-            time.sleep(0.5)
-
-    def run(self):
-        for cat in self._parse(self.url):
-            a = cat['parent_group_code']
-            prod_url: str = f'https://5ka.ru/api/v2/special_offers/?categories={a}&ordering=&page=1&price_promo__gte=&price_promo__lte=&100&search=&store'
-            prod_list1: list = []
-            for prod in self._parse1(prod_url):
-                prod_list1.append(prod)
-            prod_list: list = [{"code": cat['parent_group_code']}, {"name": cat['parent_group_name']}, {"products": prod_list1}]
-#           cat['product'] = [prod_list]
-            print (prod_list)
-            file_path = self.save_path.joinpath(f"{a}.json")
-            self._save(prod_list, file_path)
-
-    def _parse(self, url):
- #       while url:
-            response = self._get_response(url)
-            data: dict = response.json()
-            print (data)
- #          url = data["next"]
-            for pos in data:
-                yield pos
-
-    def _parse1(self, url):
-#       while url:
-        response = self._get_response(url)
+def parse_cat(url):
+        response = get_response(url)
         data: dict = response.json()
-#       url = data["next"]
-        for pos in data["results"]:
+        for pos in data:
             yield pos
 
-    def _save(self, data: dict, file_path: Path):
-        file_path.write_text(json.dumps(data, ensure_ascii=False))
+def parse_prod(url):
+    while url:
+        response = get_response(url)
+        data: dict = response.json()
+        url = data["next"]
+        for pos in data["results"]:
+            yield pos
 
 def get_save_path(dir_name: str) -> Path:
     save_path = Path(__file__).parent.joinpath(dir_name)
@@ -60,8 +29,25 @@ def get_save_path(dir_name: str) -> Path:
         save_path.mkdir()
     return save_path
 
+def save(data: dict, file_path: Path):
+    file_path.write_text(json.dumps(data, ensure_ascii=False))
+
+'''Parameters definition'''
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+}
+
 if __name__ == '__main__':
-    url = "https://5ka.ru/api/v2/categories/"
-    product_path = get_save_path('categories')
-    parser = Parse(url, product_path)
-    parser.run()
+    cat_url = 'https://5ka.ru/api/v2/categories/'
+    cat_path = get_save_path('categories')
+    '''Parsing categories'''
+    for cat in parse_cat(cat_url):
+        a = cat['parent_group_code']
+        '''Parsing products within categories'''
+        prod_url: str = f'https://5ka.ru/api/v2/special_offers/?categories={a}&ordering=&page=1&price_promo__gte=&price_promo__lte=&100&search=&store'
+        prod_list: list = []
+        for prod in parse_prod(prod_url):
+            prod_list.append(prod)
+        prod_cat: list = [{"code": cat['parent_group_code']}, {"name": cat['parent_group_name']}, {"products": prod_list}]
+        file_path = cat_path.joinpath(f"{a}.json")
+        save(prod_cat, file_path)
